2994c699c8acc99686a4c2bb35f5c918fbbbdbdd
BerezentesvEvgeny <jonn09@icloud.com>
2022-05-15 09:48:16 +0300

    Initial Commit

---
+++ Swinject_DI_Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swinject_DI_Example
+//
+//  Created by Евгений Березенцев on 15.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
